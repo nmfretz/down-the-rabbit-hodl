64e8927f1b88aaebceb21176d9eb6ca123122447
nmfretz <nmfretz@gmail.com>
2022-09-14 11:05:42 -0700

    fix svg property compatibility with react

--- components/Header.js
+++ components/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
           <button className="nav-toggle" aria-controls="primary-navigation" aria-expanded="false">
             {!isMenuOpen ? (
               <svg onClick={() => setIsMenuOpen(true)} xmlns="http://www.w3.org/2000/svg" width="25" height="21">
-                <g fill="currentColor" fill-rule="evenodd">
+                <g fill="currentColor" fillRule="evenodd">
                   <path d="M0 0h25v4H0zM0 7h25v4H0zM0 14h25v4H0z" />
                 </g>
               </svg>
@@ -37,7 +37,7 @@ const Header = () => {
               <svg onClick={() => setIsMenuOpen(false)} xmlns="http://www.w3.org/2000/svg" width="25" height="21">
                 <path
                   fill="currentColor"
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M17.925.747l2.828 2.828L13.328 11l7.425 7.425-2.828 2.828-7.425-7.425-7.425 7.425-2.828-2.828L7.671 11 .247 3.575 3.075.747 10.5 8.171 17.925.747z"
                 />
               </svg>
